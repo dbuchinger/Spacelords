f44462686c2765a11e89db360480c9721094d7e4
dbuchinger <derekbuchinger@gmail.com>
2024-01-22 20:35:46 -0600

    Changed the comet to a CharacterBody2D so that it moves based on a script instead of physics

--- Comet.gd
+++ Comet.gd
@@ -1,12 +1,13 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 var speed = 70.0
 
 func _ready():
-	linear_velocity = Vector2(-1, 1).normalized() * speed
+	velocity = Vector2(-1, 1).normalized() * speed
 	#linear_velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * speed
 
-func _on_body_entered(body):
-	speed += 2
-	linear_velocity /= (speed-2)
-	linear_velocity *= speed
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity *= 1.03
+		velocity = velocity.bounce(collision_info.get_normal())
