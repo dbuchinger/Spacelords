3ebbf88385c75ddc81616844d4e15c86f3c35c55
dbuchinger <derekbuchinger@gmail.com>
2024-02-16 11:05:46 -0600

    Added pixel art, resized the gamespace as a result, and also messed around with particles

--- Comet.gd
+++ Comet.gd
@@ -1,26 +1,27 @@
 extends CharacterBody2D
 
-var speed = 100.0
-
-var paddle_id = 28219278566
+var base_speed = 400
+var max_speed = 800
+var current_speed = base_speed
 
 func _ready():
-	velocity = Vector2(-1, .75).normalized() * speed
+	velocity = Vector2(-1, .75).normalized() * current_speed
 	#linear_velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * speed
 
 func _physics_process(delta):
+	rotation += current_speed/200 * delta
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 		if collision_info.get_collider().is_in_group("Paddles"):
 			velocity = global_position - collision_info.get_collider().global_position
-			speed = clamp(speed + 5, 100, 300)
-			velocity = velocity.normalized() * speed
+			current_speed = clamp(current_speed + 10, base_speed, max_speed)
+			velocity = velocity.normalized() * current_speed
 		elif collision_info.get_collider().is_in_group("Shields"):
-			speed = clamp(speed + 5, 100, 300)
-			velocity = velocity.normalized() * speed
+			current_speed = clamp(current_speed + 10, base_speed, max_speed)
+			velocity = velocity.normalized() * current_speed
 			velocity = velocity.bounce(collision_info.get_normal())
 			collision_info.get_collider().queue_free()
 		else: 
-			speed = clamp(speed + 5, 100, 300)
-			velocity = velocity.normalized() * speed
+			current_speed = clamp(current_speed + 10, base_speed, max_speed)
+			velocity = velocity.normalized() * current_speed
 			velocity = velocity.bounce(collision_info.get_normal())
