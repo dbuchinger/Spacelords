39b172d8dabb07c510ff5d498716c0c1f0bfdc91
dbuchinger <derekbuchinger@gmail.com>
2024-02-17 10:16:36 -0600

    Moved things around to fit the new screen size and adjusted comet speeds

--- Comet.gd
+++ Comet.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 var base_speed = 400
-var max_speed = 800
+var max_speed = 1200
 var current_speed = base_speed
+var acceleration_rate = 10
 
 func _ready():
 	velocity = Vector2(-1, .75).normalized() * current_speed
@@ -14,14 +15,17 @@ func _physics_process(delta):
 	if collision_info:
 		if collision_info.get_collider().is_in_group("Paddles"):
 			velocity = global_position - collision_info.get_collider().global_position
-			current_speed = clamp(current_speed + 10, base_speed, max_speed)
+			current_speed = clamp(current_speed + acceleration_rate, base_speed, max_speed)
 			velocity = velocity.normalized() * current_speed
+			print(current_speed)
 		elif collision_info.get_collider().is_in_group("Shields"):
-			current_speed = clamp(current_speed + 10, base_speed, max_speed)
+			current_speed = clamp(current_speed + acceleration_rate, base_speed, max_speed)
 			velocity = velocity.normalized() * current_speed
 			velocity = velocity.bounce(collision_info.get_normal())
 			collision_info.get_collider().queue_free()
+			print(current_speed)
 		else: 
-			current_speed = clamp(current_speed + 10, base_speed, max_speed)
+			current_speed = clamp(current_speed + acceleration_rate, base_speed, max_speed)
 			velocity = velocity.normalized() * current_speed
 			velocity = velocity.bounce(collision_info.get_normal())
+			print(current_speed)
