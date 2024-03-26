623285af1d56c270cf3d3981464a9be7a3096fa6
dbuchinger <derekbuchinger@gmail.com>
2024-03-25 22:10:17 -0500

    Made it so that missiles need to be active and armed to "explode", and started the respawn system for the ship.

--- Missile.gd
+++ Missile.gd
@@ -1,21 +1,30 @@
 extends Area2D
 
-var death_time = 0
-var speed = 150
+var death_time = 5000
+var speed = 50
 var velocity = Vector2.ZERO
+var active_delay = 100
+var arm_time = 1000
+@onready var sprite = $Sprite2D
+var armed_sprite = preload("res://Images/missile.png")
 
 func _ready():
-	death_time = Time.get_ticks_msec() + 5000
+	death_time += Time.get_ticks_msec()
+	active_delay += Time.get_ticks_msec()
+	arm_time += Time.get_ticks_msec()
 
 func _process(delta):
 	if Time.get_ticks_msec() >= death_time:
 		queue_free()
-
+	if Time.get_ticks_msec() >= arm_time:
+		sprite.texture = armed_sprite
+		speed = 100
+		
 func _physics_process(delta):
 	position += velocity * speed * delta
 
 func _on_body_entered(body):
-	if body.is_in_group("Shields"):
+	if Time.get_ticks_msec() >= active_delay:
+		queue_free()
+	if body.is_in_group("Shields") && Time.get_ticks_msec() >= arm_time:
 		body.queue_free()
-	
-	queue_free()
--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -7,8 +7,11 @@ var ship_aim = Vector2.ZERO
 var thrust = 10
 var drag = .1
 var bullet_reload_time = 500
-var missile_reload_time = 100
-var disabled = true
+var missile_reload_time = 300
+var disabled = false
+@export var spawn_point = global_position
+var respawn_delay = 3000
+var respawn_signal = 0
 # These are set to the negative value of the load time so that the player can
 # shoot them at the start of the game.
 var current_bullet_reload = -bullet_reload_time
@@ -29,29 +32,35 @@ var missile = preload("res://Missile.tscn")
 @export var fire_missile_action := "ship_missile1"
 
 func _process(delta):
-	if Input.is_action_pressed(fire_bullet_action):
+	if Input.is_action_pressed(fire_bullet_action) && !disabled:
 			_shoot_bullet()
-	if Input.is_action_pressed(fire_missile_action):
+	if Input.is_action_pressed(fire_missile_action) && !disabled:
 			_shoot_missile()
-
+	if disabled:
+		if Time.get_ticks_msec() >= respawn_signal:
+			visible = true
+			disabled = false
+			print("ship respawned")
+	print(respawn_signal)
 func _physics_process(delta):
-	thrust_vector = Input.get_vector(ship_left_action,ship_right_action,ship_up_action,ship_down_action)
-	thrust_vector = thrust_vector.normalized() * speed
-	
-	if thrust_vector != Vector2.ZERO:
-		ship_velocity = ship_velocity.move_toward(thrust_vector*speed, thrust*delta)
-		ship_aim = thrust_vector
-	else:
-		ship_velocity = ship_velocity.move_toward(Vector2.ZERO, drag*delta)
-	
-	look_at(global_position + thrust_vector)
-	var collision_info = move_and_collide(ship_velocity)
-	
-	if collision_info:
-		if collision_info.get_collider().is_in_group("Walls"):
-			ship_velocity = Vector2.ZERO
+	if !disabled:
+		thrust_vector = Input.get_vector(ship_left_action,ship_right_action,ship_up_action,ship_down_action)
+		thrust_vector = thrust_vector.normalized() * speed
+		
+		if thrust_vector != Vector2.ZERO:
+			ship_velocity = ship_velocity.move_toward(thrust_vector*speed, thrust*delta)
+			ship_aim = thrust_vector
 		else:
-			visible = false
+			ship_velocity = ship_velocity.move_toward(Vector2.ZERO, drag*delta)
+		
+		look_at(global_position + thrust_vector)
+		var collision_info = move_and_collide(ship_velocity)
+		
+		if collision_info:
+			if collision_info.get_collider().is_in_group("Walls"):
+				ship_velocity = Vector2.ZERO
+			else:
+				_ship_destroyed()
 
 func _shoot_bullet():
 	if Time.get_ticks_msec() > (current_bullet_reload + bullet_reload_time):
@@ -69,3 +78,10 @@ func _shoot_missile():
 		new_missile.rotation = global_rotation + PI/2 #Need to rotate them 90 degrees for whatever reason
 		game.add_child(new_missile)
 		current_missile_reload = Time.get_ticks_msec()
+
+func _ship_destroyed():
+	disabled = true
+	visible = false
+	ship_velocity = Vector2.ZERO
+	thrust_vector = Vector2.ZERO
+	respawn_signal = Time.get_ticks_msec() + respawn_delay
